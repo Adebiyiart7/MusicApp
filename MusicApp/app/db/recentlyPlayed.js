182a740e0b2d3ef2863ae6df76a5c8e85fe3ec12
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-01-19 18:24:48 +0100

    Created a fake database

---
+++ MusicApp/app/db/recentlyPlayed.js
@@ -0,0 +1,112 @@
+const songs = [
+  {
+    _id: "1",
+    title: "Asiwaju",
+    image: require("../assets/images/m1.png"),
+    link: "",
+    artist: {
+      name: "Ruger",
+      avatar: require("../assets/images/artist1.webp")
+    }
+  },
+  {
+    _id: "2",
+    title: "soso",
+    image: require("../assets/images/m1.png"),
+    link: "",
+    artist: {
+      name: "Omah Lay",
+      avatar: require("../assets/images/artist1.webp")
+    }
+  },
+  {
+    _id: "3",
+    title: "Xtra Cool",
+    image: require("../assets/images/m1.png"),
+    link: "",
+    artist: {
+      name: "Young Jonn",
+      avatar: require("../assets/images/artist1.webp")
+    }
+  },
+  {
+    _id: "4",
+    title: "Rush",
+    image: require("../assets/images/m1.png"),
+    link: "",
+    artist: {
+      name: "Ayra Starr",
+      avatar: require("../assets/images/artist1.webp")
+    }
+  },
+  {
+    _id: "5",
+    title: "How Are You (My Friend)",
+    image: require("../assets/images/m1.png"),
+    link: "",
+    artist: {
+      name: "Jonny Drille",
+      avatar: require("../assets/images/artist1.webp")
+    }
+  },
+  {
+    _id: "6",
+    title: "Common Person",
+    image: require("../assets/images/m1.png"),
+    link: "",
+    artist: {
+      name: "Burna Boy",
+      avatar: require("../assets/images/artist1.webp")
+    }
+  },
+  {
+    _id: "7",
+    title: "Girlfriend",
+    image: require("../assets/images/m1.png"),
+    link: "",
+    artist: {
+      name: "Rugar",
+      avatar: require("../assets/images/artist1.webp")
+    }
+  },
+  {
+    _id: "8",
+    title: "Soweto ft. Tempoe",
+    image: require("../assets/images/m1.png"),
+    link: "",
+    artist: {
+      name: "Victony",
+      avatar: require("../assets/images/artist1.webp")
+    }
+  },
+  {
+    _id: "9",
+    title: "Organise",
+    image: require("../assets/images/m1.png"),
+    link: "Asake",
+    artist: {
+      name: "",
+      avatar: require("../assets/images/artist1.webp")
+    }
+  },
+  {
+    _id: "10",
+    title: "For My Hand ft. Ed Sheeran",
+    image: require("../assets/images/m1.png"),
+    link: "",
+    artist: {
+      name: "Burna Boy",
+      avatar: require("../assets/images/artist1.webp")
+    }
+  },
+  {
+    _id: "11",
+    title: "Electricity",
+    image: require("../assets/images/m1.png"),
+    link: "",
+    artist: {
+      name: "Asake",
+      avatar: require("../assets/images/artist1.webp")
+    }
+  }
+];
