b6146956d067089df7a322dd2e1aa8f03e757d6a
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-01-19 13:54:45 +0100

    Working on home screen

--- MusicApp/app/screens/HomeScreen.js
+++ MusicApp/app/screens/HomeScreen.js
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import React from "react";
+import { useReducer } from "react";
 
 // LOCAL IMPORTS
 import Screen from "../components/Screen";
 import AppText from "../components/AppText";
 import AppHeader from "../components/AppHeader";
 import colors from "../config/colors";
+import ScrollableTabs from "../components/ScrollableTabs";
+import reducers from "../reducers";
+import defaultStyles from "../config/styles";
+
+const tabData = [
+  { _id: "1", name: "suggested" },
+  { _id: "2", name: "songs" },
+  { _id: "3", name: "artists" },
+  { _id: "4", name: "albums" },
+  { _id: "5", name: "favorites" }
+];
 
 const HomeScreen = ({ navigation }) => {
+  const [state, dispatch] = useReducer(reducers.scrollableTabs, {
+    active: "suggested"
+  });
+
   return (
     <Screen>
       <AppHeader
@@ -18,7 +33,11 @@ const HomeScreen = ({ navigation }) => {
         navigation={navigation}
         title={"Music"}
       />
-      <AppText>Home</AppText>
+      <ScrollableTabs data={tabData} state={state} dispatch={dispatch} />
+      <View style={defaultStyles.headingFlex}>
+        <AppText style={defaultStyles.headingFont}>Recently Played</AppText>
+        <TouchableOpacity><AppText style={defaultStyles.seeAll}>See All</AppText></TouchableOpacity>
+      </View>
     </Screen>
   );
 };
