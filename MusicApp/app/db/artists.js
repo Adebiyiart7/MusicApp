0259d87946a1b2e9396c4178a9f2ddd1fd5dd038
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-01-28 17:59:05 +0100

    Created a fake database for artists details

---
+++ MusicApp/app/db/artists.js
@@ -0,0 +1,64 @@
+
+const artists = [
+  { _id: "1", name: "Ruger", avatar: require("../assets/images/artist1.webp") },
+  {
+    _id: "2",
+    name: "Omah Lay",
+    avatar: require("../assets/images/artist2.webp")
+  },
+  {
+    _id: "3",
+    name: "Young Jonn",
+    avatar: require("../assets/images/artist3.webp")
+  },
+  {
+    _id: "4",
+
+    name: "Ayra Starr",
+    avatar: require("../assets/images/artist4.webp")
+  },
+  {
+    _id: "5",
+
+    name: "Jonny Drille",
+    avatar: require("../assets/images/artist5.webp")
+  },
+  {
+    _id: "6",
+
+    name: "Burna Boy",
+    avatar: require("../assets/images/artist6.webp")
+  },
+  {
+    _id: "7",
+
+    name: "Rugar",
+    avatar: require("../assets/images/artist6.webp")
+  },
+  {
+    _id: "8",
+
+    name: "Victony",
+    avatar: require("../assets/images/artist7.webp")
+  },
+  {
+    _id: "9",
+
+    name: "",
+    avatar: require("../assets/images/artist1.webp")
+  },
+  {
+    _id: "10",
+
+    name: "Burna Boy",
+    avatar: require("../assets/images/artist2.webp")
+  },
+  {
+    _id: "11",
+
+    name: "Asake",
+    avatar: require("../assets/images/artist3.webp")
+  }
+];
+
+export { artists };
