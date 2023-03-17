bf89cd766749a782db446ec6b9d8a3ef073c95fc
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-03-17 05:54:19 +0100

    Working on folder tab

---
+++ MusicApp/app/db/folrders.js
@@ -0,0 +1,45 @@
+const folders = [
+  {
+    _id: "1",
+    title: "Love Songs",
+    numOfSongs: 200
+  },
+
+  {
+    _id: "2",
+    title: "Christian Music",
+    numOfSongs: 200
+  },
+  {
+    _id: "3",
+    title: "Messages",
+    numOfSongs: 200
+  },
+  {
+    _id: "4",
+    title: "Rehearsal",
+    numOfSongs: 200
+  },
+  {
+    _id: "5",
+    title: "More Musics",
+    numOfSongs: 200
+  },
+  {
+    _id: "6",
+    title: "Written by Me",
+    numOfSongs: 200
+  },
+  {
+    _id: "7",
+    title: "Love Songs",
+    numOfSongs: 200
+  },
+  {
+    _id: "8",
+    title: "Love Songs",
+    numOfSongs: 200
+  }
+];
+
+export default folders;
