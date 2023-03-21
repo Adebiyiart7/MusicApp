ff695ff20a091c99c73d1315b553e858d87f1a06
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-03-21 21:45:30 +0100

    Working on playlist screen

--- MusicApp/app/screens/PlaylistScreen.js
+++ MusicApp/app/screens/PlaylistScreen.js
@@ -1,14 +1,81 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useReducer } from "react";
 
-// LOCAL IMPORT
+// LOCAL IMPORTS
 import Screen from "../components/Screen";
 import AppText from "../components/AppText";
+import AppHeader from "../components/AppHeader";
+import colors from "../config/colors";
+import ScrollableTabs from "../components/ScrollableTabs";
+import reducers from "../reducers";
+import Suggested from "../components/home/tabRenders/Suggested";
+import Songs from "../components/home/tabRenders/Songs";
+import Artists from "../components/home/tabRenders/Artists";
+import Albums from "../components/home/tabRenders/Albums";
+import Folders from "../components/home/tabRenders/Folders";
+import routes from "../config/routes";
 
-const PlaylistScreen = () => {
+export const tabData = [
+  { _id: "1", name: "suggested" },
+  { _id: "2", name: "songs" },
+  { _id: "3", name: "artists" },
+  { _id: "4", name: "albums" },
+  { _id: "5", name: "folders" }
+];
+
+const PlaylistScreen = ({ navigation }) => {
+  const [state, dispatch] = useReducer(reducers.scrollableTabs, {
+    active: "suggested"
+  });
+
+  const Render = () => {
+    if (state.active === "suggested") {
+      return <Suggested />;
+    }
+
+    if (state.active === "songs") {
+      return <Songs />;
+    }
+
+    if (state.active === "artists") {
+      return <Artists />;
+    }
+
+    if (state.active === "albums") {
+      return <Albums />;
+    }
+    
+    if (state.active === "folders") {
+      return <Folders />;
+    }
+  };
+
   return (
-    <Screen>
-      <AppText>Playlist</AppText>
+    <Screen
+      scrollable={false}
+      header={
+        <>
+          <AppHeader
+            RightIconExtra={
+              <TouchableOpacity
+                onPress={() => navigation.navigate(routes.SEARCH)}
+              >
+                <Ionicons
+                  color={colors.primaryText}
+                  size={24}
+                  name="search-outline"
+                />
+              </TouchableOpacity>
+            }
+            navigation={navigation}
+            title={"Music"}
+          />
+          <ScrollableTabs data={tabData} state={state} dispatch={dispatch} />
+        </>
+      }
+    >
+      <Render />
     </Screen>
   );
 };
