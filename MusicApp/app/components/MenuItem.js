9ed249e8f1f2adc94ae21300efe30360e03846a9
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-03-22 08:40:44 +0100

    Add menu item

---
+++ MusicApp/app/components/MenuItem.js
@@ -0,0 +1,70 @@
+import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import AppText from "./AppText";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../config/colors";
+
+const MenuItem = ({
+  title,
+  leftIcon,
+  rightIcon,
+  RightIcon,
+  subTitle,
+  isLogout,
+  onPress,
+  showRightIcon = true
+}) => {
+  return (
+    <TouchableOpacity style={styles.menuItem} onPress={onPress}>
+      <MaterialCommunityIcons
+        name={leftIcon}
+        style={[styles.leftIcon, isLogout ? { color: colors.danger } : {}]}
+      />
+      <View>
+        <Text style={[styles.title, isLogout ? { color: colors.danger } : {}]}>
+          {title}
+        </Text>
+        {subTitle && <Text style={styles.subTitle}>{subTitle}</Text>}
+      </View>
+      {showRightIcon &&
+        (RightIcon ? (
+          <AppText style={styles.rightIcon}>{RightIcon}</AppText>
+        ) : (
+          <MaterialCommunityIcons
+            style={styles.rightIcon}
+            name={rightIcon ? rightIcon : "chevron-right"}
+          />
+        ))}
+    </TouchableOpacity>
+  );
+};
+
+export default MenuItem;
+
+const styles = StyleSheet.create({
+  leftIcon: {
+    fontSize: 26,
+    marginRight: 16,
+    color: colors.primaryText
+  },
+  menuItem: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10
+  },
+  rightIcon: {
+    position: "absolute",
+    right: 0,
+    color: colors.primaryText,
+    fontSize: 26
+  },
+  subTitle: {
+  color: colors.lightText
+  },
+  title: {
+    fontWeight: "500",
+    fontSize: 16,
+    color: colors.primaryText
+  }
+});
